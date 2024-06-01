26b96fbda8dd231cec098de101f4dbecc9313d06
Jean-IG23 <116389739+Jean-IG23@users.noreply.github.com>
2024-06-01 13:37:14 -0500

    Create ejemplo2.py

---
+++ ejemplos-tuplas/ejemplo2.py
@@ -0,0 +1,8 @@
+datos = ("Juan", 30, 1.75, "Ingeniero")
+
+nombre = datos[0]
+edad = datos[1]
+altura = datos[2]
+profesion = datos[3]
+
+print(f"Nombre: {nombre}, Edad: {edad}, Altura: {altura}, Profesión: {profesion}")
