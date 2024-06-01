567c61e93016ba56a04167346dddd62a2b1e7a1d
Jean-IG23 <116389739+Jean-IG23@users.noreply.github.com>
2024-06-01 13:33:56 -0500

    Create ejemplo1.py

---
+++ ejemplos-tuplas/ejemplo1.py
@@ -0,0 +1,10 @@
+# Crear una tupla
+mi_tupla = (1, 2, 3, 4, 5)
+
+# Acceder a elementos individuales
+print("Primer elemento:", mi_tupla[0])
+print("Último elemento:", mi_tupla[-1])
+
+# Desempaquetar tupla
+a, b, c, d, e = mi_tupla
+print("Elementos desempaquetados:", a, b, c, d, e)
