0d69947ff54fbe148cbe530e1ccceb8c58d97268
Jean-IG23 <116389739+Jean-IG23@users.noreply.github.com>
2024-06-01 13:38:09 -0500

    Update ejemplo1.py

--- ejemplos-tuplas/ejemplo1.py
+++ ejemplos-tuplas/ejemplo1.py
@@ -1,7 +1,15 @@
-mi_tupla = (1, 2, 3, 4, 5)
+class Libro:
+    def __init__(self, titulo, autor, info_publicacion):
+        self.titulo = titulo
+        self.autor = autor
+        self.info_publicacion = info_publicacion
 
-print("Primer elemento:", mi_tupla[0])
-print("Último elemento:", mi_tupla[-1])
+    def detalles(self):
+        anio, editorial = self.info_publicacion
+        return f"Título: {self.titulo}\nAutor: {self.autor}\nAño: {anio}\nEditorial: {editorial}"
 
-a, b, c, d, e = mi_tupla
-print("Elementos desempaquetados:", a, b, c, d, e)
+libro1 = Libro("1984", "George Orwell", (1949, "Secker & Warburg"))
+libro2 = Libro("Cien Años de Soledad", "Gabriel García Márquez", (1967, "Editorial Sudamericana"))
+
+print(libro1.detalles())
+print(libro2.detalles())
